b765891ef31b8172564731b6f77bad296f60f68a
Marco Gosselink <44016861+MarcoGos@users.noreply.github.com>
2018-11-01 19:52:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var request = require("request");
 module.exports = function(homebridge){
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-rootedtoon", "RootedToon", RootedToon);
+  homebridge.registerAccessory("homebridge-rootedtoonmg", "RootedToon", RootedToon);
 };
 
 class RootedToon{
